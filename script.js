efff30e2c1c6615ad1a55bb6712cfa1801dc0f5f
Hyalskavran <gorniakadrian94@gmail.com>
2024-01-23 22:01:19 +0100

    project done with slight possibilities to refactor later

--- script.js
+++ script.js
@@ -40,51 +40,77 @@ let getPlayerChoice = () => {
 
 let playRound = (playerSelection, computerSelection) => {
     // Assign and make the choices both for the player and the computer
-    let p1 = playerSelection()
-    let p2 = computerSelection()
+    let p1 = playerSelection
+    let p2 = computerSelection
 
     if (p1 == p2) {
         // Tie = Replay the game
-        console.log(`Your choice: ${p1}\nComputer choice: ${p2}\nIt's a tie!`)
-        return playRound(playerSelection, computerSelection)
+        result.innerText = `It's a tie! Try again!`
+        // return playRound(playerSelection, computerSelection)
     }
     else if ((p1 == "rock" && p2 == "scissors") || (p1 == "paper" && p2 == "rock") || (p1 == "scissors" && p2 == "paper")) {
         // Check all the winning conditions and reply accordingly
-        console.log(`Your choice: ${p1}\nComputer choice: ${p2}\n${roundResult("player", p1)}`)
+        result.innerText = `${roundResult("player", p1)}`
         return "player"
     } else {
         // When lost, reply accordingly
-        console.log(`Your choice: ${p1}\nComputer choice: ${p2}\n${roundResult("computer", p2)}`)
+        result.innerText = `${roundResult("computer", p2)}`
         return "computer"
     }
 }
 
 let game = (playerSelection, computerSelection) => {
-    // Number of games won by each player
-    playerWon = 0
-    computerWon = 0
     
-    // 5 rounds total
-    for (let rounds = 0; rounds < 5; rounds++) {
-        if (playRound(playerSelection, computerSelection) == "player") {
-            playerWon++
-        } else {
-            computerWon++
-        }
-        console.log(`Player: ${playerWon}\nComputer: ${computerWon}\n`)
-    }
-    if (playerWon > computerWon) {
-        console.log(`Congratulations! You have won ${playerWon} vs ${computerWon}!`)
+    if (playRound(playerSelection, computerSelection) == "player") {
+        p1Score++
     } else {
-        console.log(`Sorry! You have lost ${computerWon} vs ${playerWon}!`)
+        p2Score++
     }
+    playerScore.innerText = `${p1Score}`
+    computerScore.innerText = `${p2Score}`
+
+    // if (playerWon > computerWon) {
+    //     console.log(`Congratulations! You have won ${playerWon} vs ${computerWon}!`)
+    // } else {
+    //     console.log(`Sorry! You have lost ${computerWon} vs ${playerWon}!`)
+    // }
 }
 
-// Get all buttons from the DOM 
-const rockBtn = document.querySelector('#rock-btn')
-const paperBtn = document.querySelector('#paper-btn')
-const scissorsBtn = document.querySelector('#scissors-btn')
+// Get all required elements from the DOM
+buttons = document.querySelectorAll('button')
+playerHand = document.querySelector('#player-hand')
+computerHand = document.querySelector('#computer-hand')
+result = document.querySelector('#result')
+playerScore = document.querySelector('#player-score')
+computerScore = document.querySelector('#computer-score')
 
+// Number of games won by each player
+let p1Score = 0
+let p2Score = 0
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        if (p1Score + p2Score < 5) {
+            playerHand.innerText = `${button.id}`
+            computerHand.innerText = `${getComputerChoice()}`
+    
+            if (playRound(playerHand.innerText, computerHand.innerText) == "player") {
+                p1Score++
+            } else {
+                p2Score++
+            }
+    
+            playerScore.innerText = p1Score
+            computerScore.innerText = p2Score
+        } else {
+            if (p1Score > p2Score) {
+                result.innerText = `Congratulations! You have won ${p1Score} vs ${p2Score}!`
+            } else {
+                result.innerText = `Sorry! You have lost ${p2Score} vs ${p1Score}!`
+            }
+        }
+    })
+});
 
 
 // Run the game!
